cd213ca2d9b93c7329ccf0c379fce3b78258caba
kushalpalesha <kpalesha@gmail.com>
2016-12-16 12:37:10 -0800

    added the ngram models generation script

---
+++ scripts/feature_calculation.py
@@ -0,0 +1,48 @@
+import pickle
+import ngram
+from feature_calculation_utils import get_feature_array, combine_datasets
+import numpy as np
+
+trigram_model = pickle.load(open("../models/trigram.model","rb"))
+quadgram_model = pickle.load(open("../models/quadgram.model","rb"))
+feature_array = get_feature_array("../domains/alexa.txt", [trigram_model, quadgram_model])
+np.save("../features/alexa", feature_array)
+
+trigram_model = pickle.load(open("../models/trigram.model","rb"))
+quadgram_model = pickle.load(open("../models/quadgram.model","rb"))
+feature_array = get_feature_array("../domains/matsnu.txt", [trigram_model, quadgram_model])
+np.save("../features/matsnu", feature_array)
+
+trigram_model = pickle.load(open("../models/trigram.model","rb"))
+quadgram_model = pickle.load(open("../models/quadgram.model","rb"))
+feature_array = get_feature_array("../domains/zeus.txt", [trigram_model, quadgram_model])
+np.save("../features/zeus", feature_array)
+
+trigram_model = pickle.load(open("../models/trigram.model","rb"))
+quadgram_model = pickle.load(open("../models/quadgram.model","rb"))
+feature_array = get_feature_array("../domains/conficker.txt", [trigram_model, quadgram_model])
+np.save("../features/conficker", feature_array)
+
+trigram_model = pickle.load(open("../models/trigram.model","rb"))
+quadgram_model = pickle.load(open("../models/quadgram.model","rb"))
+feature_array = get_feature_array("../domains/cryptolocker.txt", [trigram_model, quadgram_model])
+np.save("../features/cryptolocker", feature_array)
+
+trigram_model = pickle.load(open("../models/trigram.model","rb"))
+quadgram_model = pickle.load(open("../models/quadgram.model","rb"))
+feature_array = get_feature_array("../domains/pushdo.txt", [trigram_model, quadgram_model])
+np.save("../features/pushdo", feature_array)
+
+trigram_model = pickle.load(open("../models/trigram.model","rb"))
+quadgram_model = pickle.load(open("../models/quadgram.model","rb"))
+feature_array = get_feature_array("../domains/ramdo.txt", [trigram_model, quadgram_model])
+np.save("../features/ramdo", feature_array)
+
+trigram_model = pickle.load(open("../models/trigram.model","rb"))
+quadgram_model = pickle.load(open("../models/quadgram.model","rb"))
+feature_array = get_feature_array("../domains/tinba.txt", [trigram_model, quadgram_model])
+np.save("../features/tinba", feature_array)
+
+# combine datasets
+combine_datasets("combined",["conficker.npy","cryptolocker.npy","pushdo.npy","ramdo.npy",
+                  "matsnu.npy","rovnix.npy","tinba.npy","zeus.npy"],100000)
---
+++ scripts/ngram_model_util.py
@@ -0,0 +1,24 @@
+import ngram
+import pickle
+
+
+def generate_ngram_model(listoffilenames, N):
+    ngram_model = ngram.NGram(N=N)
+
+    for filename in listoffilenames:
+        with open(filename, "r") as text_file:
+            for domain_name in text_file:
+                second_level_domain = domain_name.split(".")[0]
+                ngram_model.add(second_level_domain)
+
+    return ngram_model
+
+# trigram model
+ngram_model = generate_ngram_model(["../domains/alexa_doNotUseForFeatureCalculation.txt"],N=3)
+filename = "../models/trigram.model"
+pickle.dump(ngram_model,open(filename,"wb"))
+
+# quadgram model
+ngram_model = generate_ngram_model(["../domains/alexa_doNotUseForFeatureCalculation.txt"],N=4)
+filename = "../models/quadgram.model"
+pickle.dump(ngram_model,open(filename,"wb"))
